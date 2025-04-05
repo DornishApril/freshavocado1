532477a6a12da84a5a73ed2bd6ca7016ced1c907
DornishApril <samireformed786@gmail.com>
2025-04-05 06:03:32 +0600

    upto 8

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+//fs is a built-in module
+
+const fs = require('fs');
+
+//use ; when declaring variables or objects
+
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+
+console.log(textIn);
+
+const textOut = `This is what we know about the avocado: ${textIn}\n${Date.now()}`;
+
+fs.writeFileSync('./txt/output.txt', textOut);
+
+console.log('File Written');
+
 arr = [1, 2, 3, 4, 5, 6]
 
 arr.map((i,j,k)=>console.log(`${i} ${j} ${k}`))
