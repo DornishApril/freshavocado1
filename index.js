b138b0a905d30079a047573248d314d743d3d128
DornishApril <samireformed786@gmail.com>
2025-04-05 13:06:53 +0600

    by the tut, cool beans

--- index.js
+++ index.js
@@ -23,11 +23,28 @@
 const fs = require('fs');
 
 //error first system
+//first parameter is error, second is data
 
-fs.readFile('./txt/start.txt','utf-8', (err,data) =>
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) =>
 {
-    console.log(data);
+    console.log(data1);
+
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) =>
+    {
+        console.log(data2);
+        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) =>
+        {
+            console.log(data3);
+            fs.writeFile('./txt/final.txt',`${data2}\n\n\n\t${data3}`, 'utf-8', err =>
+            {
+                console.log('Your file has been written 😁');
+            })
+        })
+    })
+
+    //will run AFTER the file is read;
+
     
-})
+});
 
 console.log('Will read file!');
