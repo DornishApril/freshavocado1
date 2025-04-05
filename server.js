8aae52b6e90a325a7a5201500087115d71f470d9
DornishApril <samireformed786@gmail.com>
2025-04-05 18:40:20 +0600

    12 DONE

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const http = require("http");
 const url = require("url");
 
-
 //SERVER
 
 //created server
@@ -10,20 +9,34 @@ const url = require("url");
 //res has lots of functions
 //IM SO FUCKING EXCITEDDDDDDDDDDDDDDDDDDD
 
+const server = http.createServer((req, res) => {
+  //console.log(req);
+  console.log(req.url);
 
+  const pathName = req.url;
 
-const server = http.createServer((req, res) =>
-{   
-    //console.log(req);
-    
-    console.log(`Request Recieved!`);
-    //res is RESPONDING, res is ressssinggggg after req done
-    res.end('Hellooooo FUCKERSSSS IM HEREEE NOWWWW!!! 💀');
+  if (pathName==="/"||pathName === "/overview") {
+    res.end("This is an Overview!!");
+    }
+  else if (pathName === "/product")
+  {
+      res.end("This is a product!");
+    }
+  else
+  {
+      res.writeHead(404, {
+          'Conent-type': 'text/html',
+          'my-own-header':"Hello World"
+          //an http header is a piece of info to inform the browser about the resource
 
+      });
+      res.end('Page Not Found');
+    }
+
+  console.log(`Request Recieved!`);
+  //res is RESPONDING, res is ressssinggggg after req done
 });
 
-server.listen(8000,'127.0.0.1', () =>
-{
-    console.log('Listening to requests on port 8000');
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
 });
-
