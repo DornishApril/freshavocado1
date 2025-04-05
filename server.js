a7310a2974df1592701b8f35ba3ef4b026036afc
DornishApril <samireformed786@gmail.com>
2025-04-05 18:59:39 +0600

    upto 13!!!

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const http = require("http");
+const path = require("path");
 const url = require("url");
+const fs = require("fs");
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+
+const productData = JSON.parse(data);
+console.log(productData);
+
 
 //SERVER
 
@@ -21,7 +29,23 @@ const server = http.createServer((req, res) => {
   else if (pathName === "/product")
   {
       res.end("This is a product!");
-    }
+  }
+  else if (pathName === "/api")
+  { 
+      //instead of doing ./----, using __dirname wud do good
+      //require doesnt follow it
+      
+      res.writeHead(200, {
+        'Conent-type': 'application/json',
+        'my-own-header':"Hello World"
+        //an http header is a piece of info to inform the browser about the resource
+
+    });
+
+      res.end(data);
+      
+          
+      }
   else
   {
       res.writeHead(404, {
@@ -30,7 +54,7 @@ const server = http.createServer((req, res) => {
           //an http header is a piece of info to inform the browser about the resource
 
       });
-      res.end('Page Not Found');
+      res.end('<h1>Page Not Found</h1>');
     }
 
   console.log(`Request Recieved!`);
