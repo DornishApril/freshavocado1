0c13f4ccb208c1f7f7b8dee046c0ca6ed3f14664
DornishApril <samireformed786@gmail.com>
2025-04-05 12:53:51 +0600

    vid 10, will read file before

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
-//fs is a built-in module
+// //fs is a built-in module
+
+// const fs = require('fs');
+
+// //use ; when declaring variables or objects
+// //blocking code
+// //code happens one after another
+// //SYNCHRONUS
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+
+// console.log(textIn);
+
+// const textOut = `This is what we know about the avocado: ${textIn}\n${Date.now()}`;
+
+// fs.writeFileSync('./txt/output.txt', textOut);
+
+// console.log('File Written');
+
+// arr = [1, 2, 3, 4, 5, 6]
+
+// arr.map((i,j,k)=>console.log(`${i} ${j} ${k}`))
 
 const fs = require('fs');
 
-//use ; when declaring variables or objects
-//blocking code
-//code happens one after another
-//SYNCHRONUS
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+//error first system
 
-console.log(textIn);
+fs.readFile('./txt/start.txt','utf-8', (err,data) =>
+{
+    console.log(data);
+    
+})
 
-const textOut = `This is what we know about the avocado: ${textIn}\n${Date.now()}`;
-
-fs.writeFileSync('./txt/output.txt', textOut);
-
-console.log('File Written');
-
-arr = [1, 2, 3, 4, 5, 6]
-
-arr.map((i,j,k)=>console.log(`${i} ${j} ${k}`))
+console.log('Will read file!');
